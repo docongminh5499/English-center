3713d10ed0f5b6f2841d8d0a418be89bcb9f70d4
DulioDoann <hoc.doanjos1206@hcmut.edu.vn>
2022-10-17 17:09:46 +0700

    adjust UI for timetable

--- pages/student/timetable/index.tsx
+++ pages/student/timetable/index.tsx
@@ -1,6 +1,9 @@
-import {UserRole } from "../../../helpers/constants";
+import {CookieKey, Url, UserRole } from "../../../helpers/constants";
 import { CustomNextPage } from "../../../interfaces/page.interface";
 import StudentHomeScreen from "../../../components/pageComponents/StudentScreen/StudentHomeScreen/student.home";
+import { GetServerSideProps } from "next";
+import { CookieParser } from "../../../helpers/cookieParser";
+import API from "../../../helpers/api";
 
 const StudentHomePage: CustomNextPage = (props) => {
     return <StudentHomeScreen {...props} />;
@@ -10,3 +13,16 @@ StudentHomePage.allowUsers = [
     UserRole.STUDENT,
 ];
 export default StudentHomePage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const cookies = CookieParser.parse(context.req.headers.cookie);
+    const user = cookies[CookieKey.USER] ? JSON.parse(cookies[CookieKey.USER]) : { role: UserRole.GUEST };
+    try {
+        const studentCourses = await API.get(Url.students.getTimetable, {
+            token: user.token,
+        });
+        return { props: { userRole: user.role || null, courses: studentCourses} };
+    } catch (error: any) {
+        return { props: { userRole: user.role || null, courses: null} }
+    };
+};
