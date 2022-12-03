384c570c0db38de96617ec370e7d57f1da3accad
Cong Minh Do <meo123zzz@gmail.com>
2022-12-03 20:28:36 +0700

    Change client policy security

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ import { Url, SocketBaseUrl } from "./helpers/constants";
 const securityMiddleware = [
   csp({
     directives: {
-      "default-src": ["none"],
+      "default-src": ["self"],
       "connect-src": ["self", Url.baseUrl, SocketBaseUrl, "https://www.paypal.com/sdk/js", "https://www.sandbox.paypal.com"],
       "img-src": ["self", "data:", Url.baseUrl],
       "font-src": ["self", "https://fonts.google.com", Url.baseUrl],
@@ -18,6 +18,7 @@ const securityMiddleware = [
       "form-action": ["self", Url.baseUrl],
       "object-src": ["none"],
       "base-uri": ["none"],
+      "style-src": ["self", "unsafe-inline"],
     },
   }),
   strictDynamic(),
